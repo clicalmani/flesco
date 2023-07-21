816495d0599f503cf5d552d81ca40250fdc6a542
v1p3r75 <elfriedv16@gmail.com>
2023-07-21 13:09:12 +0100

    initialized console features

---
+++ src/Console/Console.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Clicalmani\Flesco\Console;
+use Symfony\Component\Console\Application;
+
+class Console {
+
+
+    private $application = null;
+
+    public function __construct(string $appName = "Tonka Console", string $appVersion = "0.0.1") {
+
+        $this->application = new Application($appName, $appVersion);
+
+        $this->register();
+
+    }
+
+    public function register() {
+
+        $commands = require(__DIR__ . '/Kernel.php');
+
+        foreach($commands as $command) {
+            
+            $this->application->add(new $command);
+        }
+
+        $this->application->run();
+    }
+}
---
+++ src/Console/Kernel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Clicalmani\Flesco\Console;
+
+
+return [
+    
+    \Clicalmani\Flesco\Console\Commands\Controllers\Create::class,
+];
