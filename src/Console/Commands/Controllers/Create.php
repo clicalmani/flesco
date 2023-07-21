ba4606964275ea629dc286a7f7f6d11bd610dbfe
v1p3r75 <elfriedv16@gmail.com>
2023-07-21 17:03:44 +0100

    Create controller with cli

--- src/Console/Commands/Controllers/Create.php
+++ src/Console/Commands/Controllers/Create.php
@@ -3,25 +3,48 @@
 namespace Clicalmani\Flesco\Console\Commands\Controllers;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Create extends Command
 {
-    protected static $defaultName = 'controller:create';
+    protected static $defaultName = 'create:controller';
 
     protected function configure()
     {
         $this
             ->setDescription('Create a new controller')
-            ->setHelp('Aide détaillée pour ma commande')
+            ->setHelp('Create a new controller')
+            ->addArgument('name', InputArgument::REQUIRED, 'Name of the controller')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // Logique de traitement de votre commande
-        $output->writeln('Exécutée avec succès!');
-        return Command::SUCCESS;
+        $name = $input->getArgument('name');
+
+        $path = dirname(__DIR__, 8) . '/app/http/controllers/';
+        // $path = __DIR__ . "/"; // (for testing)
+
+        $file = $path . $name . '.php';
+
+        $content = file_get_contents(__DIR__ . "/Prototype.php");
+
+        if (! file_exists($file)) {
+
+            $content = str_replace('ClassName', $name, $content);
+            
+            if (file_put_contents($file, $content)) {
+
+                $output->writeln("<info>[$file] : created successfully </info>");
+
+            }
+
+            return Command::SUCCESS;
+        }
+
+        $output->writeln('<comment>File already exists !</comment>');
+        return Command::FAILURE;
     }
 }
