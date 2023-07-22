e89d814dadcd3c7f86d6c4a0002ee479a93c7fd7
honorable85 <honorable85@gmail.com>
2023-07-22 19:59:19 +0100

    Crafted Command Line Interface

---
+++ src/Console/Kernel.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * |---------------------------------------------------------------
+ * |            ***** Register Console Commands *****
+ * |---------------------------------------------------------------
+ * 
+ * Register Console commands
+ */
+
+ return [
+    \Clicalmani\Flesco\Console\Commands\Local\StartCommand::class
+ ];
---
+++ src/Maker/Application.php
@@ -0,0 +1,22 @@
+<?php
+namespace Clicalmani\Flesco\Maker;
+
+use Symfony\Component\Console\Command\Command;
+
+class Application extends \Symfony\Component\Console\Application
+{
+    function __construct(private $root_pah = null)
+    {
+        parent::__construct();
+    }
+
+    function make()
+    {
+        // Console Kernel
+        $kernel = require_once dirname(__DIR__) . '/Console/Kernel.php';
+
+        foreach ($kernel as $command) {
+            $this->add(new $command);
+        }
+    }
+}
